e2a62557e326785a7bfe79df0141f6b49595c12c
ethicsense <gycksdl94@gmail.com>
2023-10-12 14:10:27 +0900

    EVC deployment test proceeding #2

--- run.py
+++ run.py
@@ -5,5 +5,17 @@ from server import app
 
 
 if __name__ == "__main__":
-    
-    app.run(host='0.0.0.0', port=5454)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--server_name',
+        type=str,
+        default="0.0.0.0"
+    )
+    parser.add_argument(
+        '--server_port',
+        type=int,
+        default=7860
+    )
+
+    app.run(host=args.server_name, port=args.server_port)
